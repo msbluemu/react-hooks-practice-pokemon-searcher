0b29cda1de30682f482f77f8ff3e36195af02a30
Qianyun Mu <msbluemu@gmail.com>
2023-07-19 11:51:44 +1000

    initial commit

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
+
 function PokemonPage() {
+  const [pokemons, setPokemons] = useState([]);
+  const [filteredPokemons, setFilteredPokemons] = useState([]);
+
+
+  useEffect(()=> {
+    fetch("  http://localhost:3001/pokemon")
+     .then((r) => r.json())
+     .then((data) => {
+      setPokemons(data);
+      setFilteredPokemons(data);
+     })
+  }, [])
+
+  const handleSearch = (searchQuery) => {
+    const filtered = pokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredPokemons(filtered);
+  };
+
   return (
-    <Container>
+    <Container >
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search onSearch={handleSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={filteredPokemons} setPokemons={setPokemons}/>
     </Container>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-function Search() {
+function Search({ onSearch }) {
+  const [searchQuery, setSearchQuery] = useState("");
+  
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearchQuery(query);
+    onSearch(query)
+  };
+ 
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" 
+               value={searchQuery}
+               onChange={handleSearch}
+          />
         <i className="search icon" />
       </div>
     </div>
